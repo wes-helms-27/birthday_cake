96b61cb7c8c563133a1538cd3a2271aa19543e5b
carpentt27 <carpentt27@up.edu>
2024-02-20 10:17:01 -0800

    Changed frosting color to velvet

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -47,7 +47,7 @@ public class CakeView extends SurfaceView {
         setWillNotDraw(false);
 
         //Setup our palette
-        cakePaint.setColor(0xFFC755B5);  //violet-red
+        cakePaint.setColor(0xFF9C0000);  //violet-red
         cakePaint.setStyle(Paint.Style.FILL);
         frostingPaint.setColor(0xFFFFFACD);  //pale yellow
         frostingPaint.setStyle(Paint.Style.FILL);
